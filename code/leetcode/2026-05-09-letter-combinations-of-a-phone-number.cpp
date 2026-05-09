51cf202680a87bd33252a382638f959d5cd96d83
condercx <2817529759@qq.com>
2026-05-09 16:37:24 +0800

    W9: 完成今天的代码随想录

--- code/leetcode/2026-05-09-letter-combinations-of-a-phone-number.cpp
+++ code/leetcode/2026-05-09-letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
+private:
+	const string letterMap[10] = {
+			"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
+		};
 public:
+	vector<string> res;
+	string s;
+	void backtrack(const string &digits, int index) {
+		if (index == digits.size()) {
+			res.push_back(s);
+			return;
+		}
+
+		int digit = digits[index] - '0';
+		string letters = letterMap[digit];
+		for (int i = 0; i < letters.size(); i++) {
+			s.push_back(letters[i]);
+			backtrack(digits, index + 1);
+			s.pop_back();
+		}
+	}
 	vector<string> letterCombinations(string digits) {
-		
+		backtrack(digits, 0);
+
+		return res;
 	}
 };
